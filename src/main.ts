9e7a70356de21f6808ea45f510d19a8542be4afe
dat <dat.nv1506@gmail.com>
2024-11-11 22:05:23 +0700

    first commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import Antd from 'ant-design-vue'
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './style.css'
+import 'ant-design-vue/dist/reset.css'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Antd)
+
+app.mount('#app')
