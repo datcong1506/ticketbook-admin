b555bb2524adc2b3b4a9ed11e91d7c974f043ac8
dat <dat.nv1506@gmail.com>
2024-11-19 13:57:00 +0700

    unresovled component plugin

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,12 +1,25 @@
 <template>
-  <div class="absolute w-[375px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl flex-col gap-4 bg-[white] overflow-hidden">
+  <div class="absolute w-[375px] top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 rounded-xl flex flex-col gap-4 bg-[white] overflow-hidden border-[rgb(45,_194,_117)]">
     <div class="h-[112px] bg-[rgb(45,_194,_117)] relative">
-      <div>
+      <div class="absolute bottom-0 left-[10%] h-[64px] w-[85%] text-3xl font-bold text-white flex items-center">
         Login
       </div>
-      <div />
     </div>
-    <div />
+    <div class="flex flex-col gap-4 px-5 pb-5">
+      <a-form>
+        <a-form-item :rules="[{ required: true, message: 'Please input your username!' }]">
+          <a-input placeholder="Please input your username" />
+        </a-form-item>
+        <a-form-item :rules="[{ required: true, message: 'Please input your password!' }]">
+          <a-input-password placeholder="Please input your password" />
+        </a-form-item>
+        <a-form-item>
+          <a-button :disabled="false" type="primary">
+            Submit
+          </a-button>
+        </a-form-item>
+      </a-form>
+    </div>
   </div>
 </template>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,24 @@
 import path from 'node:path'
 import vue from '@vitejs/plugin-vue'
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
+import Components from 'unplugin-vue-components/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [AntDesignVueResolver({
+        importStyle: false,
+      })],
+      dts: true, // Generate type definitions
+    }),
+  ],
   resolve: {
     alias: {
       '@views': path.resolve(__dirname, 'src/views'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
 })
